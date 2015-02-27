651176fad51a1a50b0f17f3d4000ec7f0dab091c
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-02-27 15:42:03 +0300

    referee tests

---
+++ verification/src/referee.py
@@ -0,0 +1,23 @@
+from checkio_referee import RefereeBase
+from checkio_referee.covercode import py_unwrap_args
+
+import settings
+import settings_env
+from tests import TESTS
+
+cover = """
+def cover(f, data):
+    f(*[tuple(str(row) for row in matrix) for matrix in data])
+"""
+
+
+class Referee(RefereeBase):
+    TESTS = TESTS
+    EXECUTABLE_PATH = settings.EXECUTABLE_PATH
+    CURRENT_ENV = settings_env.CURRENT_ENV
+    FUNCTION_NAME = "recall_password"
+    ENV_COVERCODE = {
+        "python_2": cover,
+        "python_3": cover,
+        "javascript": None
+    }
